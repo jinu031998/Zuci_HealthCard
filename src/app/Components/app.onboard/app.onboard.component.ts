8b00192adcbf094bcc5eebe1f939ba3b9b5b8945
JiNu0001 <58718855+JiNu0001@users.noreply.github.com>
2021-09-20 00:53:18 +0530

    ChangeDetectionStrategy.OnPush

--- src/app/Components/app.onboard/app.onboard.component.ts
+++ src/app/Components/app.onboard/app.onboard.component.ts
@@ -11,7 +11,7 @@ import { map } from 'rxjs/operators';
   selector: 'hc-app-onboard',
   templateUrl: './app.onboard.component.html',
   styleUrls: ['./app.onboard.component.css'],
-  changeDetection: ChangeDetectionStrategy.Default
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppOnboardComponent implements OnInit {
   constructor(private accountService : AccountService, private formBuilder : FormBuilder){}
